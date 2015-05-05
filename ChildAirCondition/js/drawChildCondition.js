24f80fc0a7634ae248a75f80edaf1fafde8a5c78
梁家瑞 <ljracm@gmail.com>
2015-05-05 21:54:06 +0800

    测试鼠标点击事件-开关机

--- ChildAirCondition/js/drawChildCondition.js
+++ ChildAirCondition/js/drawChildCondition.js
@@ -1,6 +1,12 @@
+/**
+ * @author ljracm@gmail.com
+ *
+ * @file 从控机
+ */
 var canvas = document.getElementById('child-condition');
 canvas.width = 1200;
 canvas.height = 800;
+
 // 空调外壳画布
 var AirConditionBox = canvas.getContext('2d');
 AirConditionBox.fillStyle = '#FF0000';
@@ -8,20 +14,14 @@ AirConditionBox.fillRect(0, 0, 1200, 800);
 
 // 空调显示屏
 var AirConditionScreen = canvas.getContext('2d');
-AirConditionScreen.fillStyle = '#8F8FB2';
+AirConditionScreen.fillStyle = '#000000';
 AirConditionScreen.fillRect(50, 50, 900, 600);
 
 // 温度显示
 var TempWord = canvas.getContext('2d');
-TempWord.fillStyle = '#000000';
-TempWord.font = '40px Arial';
-TempWord.fillText('制冷温度：25 ℃      缺省温度：25 ℃', 65, 90);
 
 // 风速显示
 var WindWord = canvas.getContext('2d');
-WindWord.fillStyle = '#000000';
-WindWord.font = '40px Arial';
-WindWord.fillText('风速：低        缺省风速：低', 65, 140);
 
 // 空调风速设置
 var WindBox = canvas.getContext('2d');
@@ -32,15 +32,15 @@ var WindUp = canvas.getContext('2d');
 WindUpImg.src = 'asset/up.png';
 WindUpImg.onload = function () {
     WindUp.drawImage(WindUpImg, 0, 0, 64, 64, 1110, 50, 64, 64);
-}
+};
 var WindDownImg = document.createElement('img');
 var WindDown = canvas.getContext('2d');
 WindDownImg.src = 'asset/down.png';
 WindDownImg.onload = function () {
     WindDown.drawImage(WindDownImg, 0, 0, 64, 64, 1110, 114, 64, 64);
-}
+};
 
-//温度调节设置
+// 温度调节设置
 var TempBox = canvas.getContext('2d');
 TempBox.fillStyle = '#66FF66';
 TempBox.fillRect(980, 200, 120, 120);
@@ -49,26 +49,62 @@ var TempUp = canvas.getContext('2d');
 TempUpImg.src = 'asset/up.png';
 TempUpImg.onload = function () {
     TempUp.drawImage(TempUpImg, 0, 0, 64, 64, 1110, 200, 64, 64);
-}
+};
 var TempDownImg = document.createElement('img');
 var TempDown = canvas.getContext('2d');
 TempDownImg.src = 'asset/down.png';
 TempDownImg.onload = function () {
     TempDown.drawImage(TempDownImg, 0, 0, 64, 64, 1110, 264, 64, 64);
-}
+};
 
-//开关
+// 开关
 var SwitchImg = document.createElement('img');
 var Switch = canvas.getContext('2d');
+Switch.isOpen = false;
 SwitchImg.src = 'asset/switch.png';
 SwitchImg.onload = function () {
     Switch.drawImage(SwitchImg, 0, 0, 128, 128, 1000, 650, 128, 128);
-}
+};
 
-//费用查询
+// 费用查询
 var CheckInImg = document.createElement('img');
 var CheckIn = canvas.getContext('2d');
 CheckInImg.src = 'asset/checkin.png';
 CheckInImg.onload = function () {
-    Switch.drawImage(CheckInImg, 0, 0, 128, 128, 50, 670, 100, 100);
-}
+    CheckIn.drawImage(CheckInImg, 0, 0, 128, 128, 50, 670, 100, 100);
+};
+
+canvas.onclick = function (event) {
+    var x = event.pageX - canvas.offsetLeft;
+    var y = event.pageY - canvas.offsetTop;
+    // 开关机
+    if (x >= 1000 && y >= 650 && x <= 1128 && y <= 778) {
+        debugger;
+        if (Switch.isOpen) {
+            AirConditionScreen.shut();
+            Switch.isOpen = false;
+        }
+        else {
+            AirConditionScreen.show();
+            Switch.isOpen = true;
+        }
+    }
+};
+
+AirConditionScreen.show = function () {
+    AirConditionScreen.fillStyle = '#66FF66';
+    AirConditionScreen.fillRect(50, 50, 900, 600);
+    // 温度显示
+    TempWord.fillStyle = '#000000';
+    TempWord.font = '40px Arial';
+    TempWord.fillText('制冷温度：25 ℃      缺省温度：25 ℃', 65, 90);
+    // 风速显示
+    WindWord.fillStyle = '#000000';
+    WindWord.font = '40px Arial';
+    WindWord.fillText('风速：低        缺省风速：低', 65, 140);
+};
+
+AirConditionScreen.shut = function () {
+    AirConditionScreen.fillStyle = '#000000';
+    AirConditionScreen.fillRect(50, 50, 900, 600);
+};
