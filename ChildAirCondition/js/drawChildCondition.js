5fc3fffd01bbf106cfd5f9fd2ef8d38901c8f9e4
梁家瑞 <ljracm@gmail.com>
2015-05-10 00:51:13 +0800

    重构原cavas的子控件，把子canvas的尺寸全写成变量配置

--- ChildAirCondition/js/drawChildCondition.js
+++ ChildAirCondition/js/drawChildCondition.js
@@ -9,82 +9,177 @@ var canvas = document.getElementById('child-condition');
 canvas.width = 1200;
 canvas.height = 800;
 
+// 空调布局参数配置
+var AirConditionBox = {
+    x: 0,
+    y: 0,
+    width: 1200,
+    height: 800,
+    fillStyle: '#FF0000'
+};
+
+var AirConditionScreen = {
+    x: 50,
+    y: 50,
+    width: 900,
+    height: 600,
+    fillOnStyle: '#66FF66',
+    fillShutStyle: '#000000'
+};
+
+var TempBox = {
+    x: 980,
+    y: 200,
+    width: 120,
+    height: 120,
+    PictureWidth: 64,
+    PictureHeight: 64
+};
+
+var TempWord = {
+    x: 65,
+    y: 90,
+    fillStyle: '#000000',
+    font: '40px Arial'
+};
+
+var TempUp = {
+    x: 1110,
+    y: 200,
+    width: 64,
+    height: 64,
+    PictureWidth: 64,
+    PictureHeight: 64
+};
+
+var TempDown = {
+    x: 1110,
+    y: 264,
+    width: 64,
+    height: 64,
+    PictureWidth: 64,
+    PictureHeight: 64
+};
+
+var WindBox = {
+    x: 980,
+    y: 50,
+    width: 120,
+    height: 120,
+    fillOnStyle: '#66FF66',
+    fillShutStyle: '#000000'
+};
+
+var WindWord = {
+    x: 65,
+    y: 140,
+    fillStyle: '#000000',
+    font: '40px Arial'
+};
+
+var WindUp = {
+    x: 1110,
+    y: 50,
+    width: 64,
+    height: 64,
+    PictureWidth: 64,
+    PictureHeight: 64
+};
+
+var WindDown = {
+    x: 1110,
+    y: 114,
+    width: 64,
+    height: 64,
+    PictureWidth: 64,
+    PictureHeight: 64
+};
+
+var Switch = {
+    x: 1000,
+    y: 650,
+    width: 128,
+    height: 128,
+    PictureWidth: 128,
+    PictureHeight: 128
+};
+
+var CheckIn = {
+    x: 50,
+    y: 670,
+    width: 100,
+    height: 100,
+    PictureWidth: 128,
+    PictureHeight: 128
+};
+
 // 空调外壳画布
-var AirConditionBox = canvas.getContext('2d');
-AirConditionBox.defaultTemp = 25;
-AirConditionBox.defaultWind = 0;
-AirConditionBox.curTemp;
-AirConditionBox.curWind;
-AirConditionBox.fillStyle = '#FF0000';
-AirConditionBox.fillRect(0, 0, 1200, 800);
+var AirCondition = canvas.getContext('2d');
+AirCondition.defaultTemp = 25;
+AirCondition.defaultWind = 0;
+AirCondition.curTemp;
+AirCondition.curWind;
+AirCondition.fillStyle = AirConditionBox.fillStyle;
+AirCondition.fillRect(AirConditionBox.x, AirConditionBox.y,  AirConditionBox.width, AirConditionBox.height);
 
 // 空调显示屏
-var AirConditionScreen = canvas.getContext('2d');
-AirConditionScreen.fillStyle = '#000000';
-AirConditionScreen.fillRect(50, 50, 900, 600);
-
-// 温度显示
-var TempWord = canvas.getContext('2d');
-
-// 风速显示
-var WindWord = canvas.getContext('2d');
+AirCondition.fillStyle = AirConditionScreen.fillShutStyle;
+AirCondition.fillRect(AirConditionScreen.x, AirConditionScreen.y, AirConditionScreen.width, AirConditionScreen.height);
 
 // 空调风速设置
-var WindBox = canvas.getContext('2d');
-WindBox.fillStyle = '#000000';
-WindBox.fillRect(980, 50, 120, 120);
+AirCondition.fillStyle = WindBox.fillShutStyle;
+AirCondition.fillRect(WindBox.x, WindBox.y, WindBox.width, WindBox.height);
 var WindUpImg = document.createElement('img');
-var WindUp = canvas.getContext('2d');
 WindUpImg.src = 'asset/up.png';
 WindUpImg.onload = function () {
-    WindUp.drawImage(WindUpImg, 0, 0, 64, 64, 1110, 50, 64, 64);
+    AirCondition.drawImage(WindUpImg, 0, 0, WindUp.PictureWidth, WindUp.PictureHeight,
+                           WindUp.x, WindUp.y, WindUp.width, WindUp.height);
 };
 var WindDownImg = document.createElement('img');
-var WindDown = canvas.getContext('2d');
 WindDownImg.src = 'asset/down.png';
 WindDownImg.onload = function () {
-    WindDown.drawImage(WindDownImg, 0, 0, 64, 64, 1110, 114, 64, 64);
+    AirCondition.drawImage(WindDownImg, 0, 0, WindDown.PictureWidth, WindDown.PictureHeight,
+                           WindDown.x, WindDown.y, WindDown.width, WindDown.height);
 };
 
 // 温度调节设置
-var TempBox = canvas.getContext('2d');
-TempBox.fillStyle = '#000000';
-TempBox.fillRect(980, 200, 120, 120);
+AirCondition.fillStyle = TempBox.fillShutStyle;
+AirCondition.fillRect(TempBox.x, TempBox.y, TempBox.width, TempBox.height);
 var TempUpImg = document.createElement('img');
-var TempUp = canvas.getContext('2d');
 TempUpImg.src = 'asset/up.png';
 TempUpImg.onload = function () {
-    TempUp.drawImage(TempUpImg, 0, 0, 64, 64, 1110, 200, 64, 64);
+    AirCondition.drawImage(TempUpImg, 0, 0, TempUp.PictureWidth, TempUp.PictureHeight,
+                           TempUp.x, TempUp.y, TempUp.width, TempUp.height);
 };
 var TempDownImg = document.createElement('img');
-var TempDown = canvas.getContext('2d');
 TempDownImg.src = 'asset/down.png';
 TempDownImg.onload = function () {
-    TempDown.drawImage(TempDownImg, 0, 0, 64, 64, 1110, 264, 64, 64);
+    AirCondition.drawImage(TempDownImg, 0, 0, TempDown.PictureWidth, TempDown.PictureHeight,
+                       TempDown.x, TempDown.y, TempDown.width, TempDown.height);
 };
 
 // 开关
 var SwitchImg = document.createElement('img');
-var Switch = canvas.getContext('2d');
 Switch.isOpen = false;
 SwitchImg.src = 'asset/switch.png';
 SwitchImg.onload = function () {
-    Switch.drawImage(SwitchImg, 0, 0, 128, 128, 1000, 650, 128, 128);
+    AirCondition.drawImage(SwitchImg, 0, 0, Switch.PictureWidth, Switch.PictureHeight,
+                           Switch.x, Switch.y, Switch.width, Switch.height);
 };
 
 // 费用查询
 var CheckInImg = document.createElement('img');
-var CheckIn = canvas.getContext('2d');
 CheckInImg.src = 'asset/checkin.png';
 CheckInImg.onload = function () {
-    CheckIn.drawImage(CheckInImg, 0, 0, 128, 128, 50, 670, 100, 100);
+    AirCondition.drawImage(CheckInImg, 0, 0, CheckIn.PictureWidth, CheckIn.PictureHeight,
+                           CheckIn.x, CheckIn.y, CheckIn.width, CheckIn.height);
 };
 
 canvas.onclick = function (event) {
     var x = event.pageX - canvas.offsetLeft;
     var y = event.pageY - canvas.offsetTop;
     // 开关机
-    if (x >= 1000 && y >= 650 && x <= 1128 && y <= 778) {
+    if (x >= Switch.x && y >= Switch.y && x <= Switch.x + Switch.width && y <= Switch.y + Switch.height) {
         if (Switch.isOpen) {
             AirConditionScreen.shut();
             Switch.isOpen = false;
@@ -94,49 +189,51 @@ canvas.onclick = function (event) {
             Switch.isOpen = true;
         }
     }
-    else if (x >= 1110 && y >= 50 && x <= 1174 && y <= 114) {
+    else if (x >= WindUp.x && y >= WindUp.y && x <= WindUp.x + WindUp.width && y <= WindUp.y + WindUp.height) {
         var t = AirConditionBox.curTemp || AirConditionBox.defaultTemp;
     }
 };
 
 AirConditionScreen.show = function () {
-    var t = AirConditionBox.defaultTemp;
-    var w = AirConditionBox.defaultWind;
-    AirConditionScreen.fillStyle = '#66FF66';
-    AirConditionScreen.fillRect(50, 50, 900, 600);
-    WindBox.fillRect(980, 50, 120, 120);
-    TempBox.fillRect(980, 200, 120, 120);
+    var t = AirCondition.defaultTemp;
+    var w = AirCondition.defaultWind;
+    AirCondition.fillStyle = AirConditionScreen.fillOnStyle;
+    AirCondition.fillRect(AirConditionScreen.x, AirConditionScreen.y,
+                          AirConditionScreen.width, AirConditionScreen.height);
+    AirCondition.fillRect(WindBox.x, WindBox.y, WindBox.width, WindBox.height);
+    AirCondition.fillRect(TempBox.x, TempBox.y, TempBox.width, TempBox.height);
 
     // 温度显示
-    TempWord.fillStyle = '#000000';
-    TempWord.font = '40px Arial';
+    AirCondition.fillStyle = TempWord.fillStyle;
+    AirCondition.font = TempWord.font;
     var str = '制冷温度：'
             + t
             + ' ℃      缺省温度：'
             + t
             + '℃';
-    TempWord.fillText(str, 65, 90);
+    AirCondition.fillText(str, TempWord.x, TempWord.y);
     // 风速显示
-    WindWord.fillStyle = '#000000';
-    WindWord.font = '40px Arial';
+    AirCondition.fillStyle = WindWord.fillStyle;
+    AirCondition.font = WindWord.font;
     str = '风速：'
             + WindDescrib[w]
             + '      缺省风速：'
             + WindDescrib[w];
-    WindWord.fillText(str, 65, 140);
+    AirCondition.fillText(str, WindWord.x, WindWord.y);
     // 调节窗口显示
-    WindBox.fillStyle = '#000000';
-    WindBox.font = '40px Arial';
-    WindBox.fillText(WindDescrib[w], 1020, 125);
+    AirCondition.fillStyle = WindWord.fillStyle;
+    AirCondition.font = WindWord.font;
+    AirCondition.fillText(WindDescrib[w], 1020, 125);
     str = t
         + '℃';
-    TempBox.fillText(str, 1000, 270);
+    AirCondition.fillText(str, 1000, 270);
 };
 
 AirConditionScreen.shut = function () {
-    AirConditionScreen.fillStyle = '#000000';
-    AirConditionScreen.fillRect(50, 50, 900, 600);
-    WindBox.fillRect(980, 50, 120, 120);
-    TempBox.fillRect(980, 200, 120, 120);
+    AirCondition.fillStyle = '#000000';
+    AirCondition.fillRect(AirConditionScreen.x, AirConditionScreen.y,
+                          AirConditionScreen.width, AirConditionScreen.height);
+    AirCondition.fillRect(WindBox.x, WindBox.y, WindBox.width, WindBox.height);
+    AirCondition.fillRect(TempBox.x, TempBox.y, TempBox.width, TempBox.height);
 };
 
